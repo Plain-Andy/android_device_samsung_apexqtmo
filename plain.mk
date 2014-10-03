bc1a338af3193a0ef02367e467199008b38985c9
TheREV3N <rev3nt3ch@gmail.com>
2014-10-02 22:40:12 -0700

    Update and rename liquid.mk to plain.mk

--- plain.mk
+++ plain.mk
@@ -1,15 +1,18 @@
 $(call inherit-product, device/samsung/apexqtmo/full_apexqtmo.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
+# Inherit some common plain stuff
+$(call inherit-product, vendor/plain/config/common.mk)
 
-# Inherit some common liquid stuff.
-$(call inherit-product, vendor/liquid/config/common_phone.mk)
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/plain/config/gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/plain/config/nfc_enhanced.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=apexqtmo TARGET_DEVICE=apexqtmo BUILD_FINGERPRINT="samsung/apexqtmo/apexqtmo:4.1.2/JZO54K/T699UVBMC5:user/release-keys" PRIVATE_BUILD_DESC="apexqtmo-user 4.1.2 JZO54K T699UVBMC5 release-keys"
 
 
-PRODUCT_NAME := liquid_apexqtmo
+PRODUCT_NAME := plain_apexqtmo
 PRODUCT_DEVICE := apexqtmo
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SGH-T699
