34dce8b85b6479e55fff5e1143bfdb6ed5ed58c5
Snuzzo <jrracinfan@gmail.com>
2014-10-05 18:31:38 -0400

    Updated dependencies/Add bootanimation

--- plain.mk
+++ plain.mk
@@ -9,11 +9,14 @@ $(call inherit-product, vendor/plain/config/gsm.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/plain/config/nfc_enhanced.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=apexqtmo TARGET_DEVICE=apexqtmo BUILD_FINGERPRINT="samsung/apexqtmo/apexqtmo:4.1.2/JZO54K/T699UVBMC5:user/release-keys" PRIVATE_BUILD_DESC="apexqtmo-user 4.1.2 JZO54K T699UVBMC5 release-keys"
-
-
 PRODUCT_NAME := plain_apexqtmo
 PRODUCT_DEVICE := apexqtmo
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SGH-T699
 PRODUCT_MANUFACTURER := samsung
+
+# Boot animation
+PRODUCT_COPY_FILES += \
+    vendor/plain/prebuilt/bootanimation/480.zip:system/media/bootanimation.zip
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=apexqtmo TARGET_DEVICE=apexqtmo BUILD_FINGERPRINT="samsung/apexqtmo/apexqtmo:4.1.2/JZO54K/T699UVBMC5:user/release-keys" PRIVATE_BUILD_DESC="apexqtmo-user 4.1.2 JZO54K T699UVBMC5 release-keys"
